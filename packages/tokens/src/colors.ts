efbe9eefafd9698db82b6f54c8483acb5fc7ee30
matheusbarcc <matheusbarcon@gmail.com>
2025-02-18 07:56:50 -0300

    First commit

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  white: '#FFF',
+
+  cornsilk100: '#4F4C42',
+  cornsilk200: '#666256',
+  cornsilk300: '#827E6D',
+  cornsilk400: '#A19B87',
+  cornsilk500: '#E3DBBD',
+  cornsilk600: '#F6EDCB',
+  cornsilk700: '#FFF6D8',
+
+  blueDark: '#66ABA5',
+  blueMid: '#9AC8C4',
+  blueLight: '#CCEFEC',
+
+  orangeDark: '#F1B04E',
+  orangeMid: '#EFC482',
+  orangeLight: '#EED8B6',
+
+  redDark: '#E26B5A',
+  redMid: '#E99A8F',
+  redLight: '#EFBFB8',
+}
---
+++ packages/tokens/src/font-sizes.ts
@@ -0,0 +1,9 @@
+export const fontSizes = {
+  xxs: '0.625rem',
+  xs: '0.75rem',
+  sm: '0.875rem',
+  md: '1rem',
+  lg: '1.25rem',
+  xl: '2rem',
+  '2xl': '2.5rem',
+}
